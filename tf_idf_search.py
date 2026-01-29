54d018b26ae99c52d2d81d55837824f308f6f4a6
nesterenkojul <julia.nesterenko21@gmail.com>
2026-01-30 00:37:22 +0200

    #19 Modified the tf-idf to support the multi-word search (+ lemmatization/stemming)

--- tf_idf_search.py
+++ tf_idf_search.py
@@ -1,7 +1,15 @@
-# tf-idf: relevance-ranked search 
+# tf-idf: relevance-ranked search
 import numpy as np
 import os
+import nltk
+from math import inf
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+stemmer = nltk.stem.SnowballStemmer('english') # supports finnish and swedish as well
+nltk.download("wordnet")
+lemmatizer = nltk.wordnet.WordNetLemmatizer()
+
 
 def load_documents(filename):
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -12,15 +20,35 @@ def load_documents(filename):
         documents = [' '.join(doc.strip().split("\n")[1:]) for doc in documents]
         return documents
 
+
 def first_words_in_doc(file):
     words = " ".join(file.split()[:50])
     return words
 
+
+def extract_stems(docs):
+    stem_sentence = lambda doc: ' '.join([stemmer.stem(word)for word in doc.lower().split()])
+    if isinstance(docs, str):
+        return stem_sentence(docs)
+    return [stem_sentence(doc) for doc in docs]
+
+
+def extract_lemmas(docs):
+    lem_sentence = lambda doc: ' '.join([lemmatizer.lemmatize(word)for word in doc.lower().split()])
+    if isinstance(docs, str):
+        return lem_sentence(docs)
+    return [lem_sentence(doc) for doc in docs]
+
+
 def tf_idf_search(query, file):
+    ngram_size = len(query.split())
+    # stemmed_query = extract_stems(query)
+    lemmatized_query = extract_lemmas(query)
+
     # Vectorize query string
-    tfv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2")
+    tfv = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2", ngram_range=(ngram_size, ngram_size)) # ngram_range=(1, ngram_size) for a non-strict search
     tf_matrix = tfv.fit_transform(file).T.tocsr()
-    query_vec = tfv.transform([query]).tocsc()
+    query_vec = tfv.transform([lemmatized_query]).tocsc()
 
     # Cosine similarity
     hits = np.dot(query_vec, tf_matrix) 
@@ -36,6 +64,7 @@ def tf_idf_search(query, file):
         print("Doc #{:d} (score: {:.4f})".format(i, score))
         print(first_words_in_doc(file[doc_idx]), "\n")
 
+
 def main():
     file = "enwiki-20181001-corpus.1000-articles.txt"
     file = load_documents(file)
@@ -43,5 +72,6 @@ def main():
     query = input("Please enter an query:")
     
     tf_idf_search(query, file)
+
 
 main()
